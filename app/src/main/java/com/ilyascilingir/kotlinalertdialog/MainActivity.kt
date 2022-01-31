d2b710d18a7be23d41776609dab8cd364b829334
İlyas Çilingir <ilyas.cilingir@icisleri.gov.tr>
2022-01-31 11:52:37 +0300

    Initial commit

---
+++ app/src/main/java/com/ilyascilingir/kotlinalertdialog/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ilyascilingir.kotlinalertdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "Welcome", Toast.LENGTH_LONG).show()
+    }
+
+    fun save (view : View) {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("Save")
+        alert.setMessage("Are you sure ?")
+        alert.setPositiveButton("Yes") {dialog, which ->
+            Toast.makeText(applicationContext,"Saved",Toast.LENGTH_LONG).show()
+        }
+        alert.setNegativeButton("No") {dialog, which ->
+            Toast.makeText(applicationContext,"Not saved",Toast.LENGTH_LONG).show()
+        }
+        alert.show()
+    }
+}
